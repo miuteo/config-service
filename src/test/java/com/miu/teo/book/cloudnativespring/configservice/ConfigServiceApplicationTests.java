e76e444935a4f607554a02f42acb46c427544257
miuteo <teodor.miu@outlook.com>
2025-02-11 22:24:06 +0200

    init

---
+++ src/test/java/com/miu/teo/book/cloudnativespring/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.teo.book.cloudnativespring.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
